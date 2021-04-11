2aae59b3ac0a926b28bb97c46588141cb1e1bf46
Matt Watson <mattwat@gmail.com>
2021-04-11 00:59:25 -0400

    Updates

---
+++ precache-manifest.3f8c8b47b16bd34bd142049118c48948.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbebaeb33ad5c17a6cf87902266136d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49aabd54c32536d0b455",
+    "url": "./static/css/main.d4052c8e.chunk.css"
+  },
+  {
+    "revision": "84b939331b82d6e6d7cb",
+    "url": "./static/js/2.d681c10d.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.d681c10d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49aabd54c32536d0b455",
+    "url": "./static/js/main.2051947c.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.2051947c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8899f00d1e00b9b35ca9",
+    "url": "./static/js/runtime-main.4e6de7fe.js"
+  }
+]);
